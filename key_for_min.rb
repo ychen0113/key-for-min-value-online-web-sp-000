042a6f919cacf1a683c88572a11196268173d55b
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-29 20:41:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-def a 
+name_hash.each do |key, value|
+  a = key
+  b = value 
+  
 end
